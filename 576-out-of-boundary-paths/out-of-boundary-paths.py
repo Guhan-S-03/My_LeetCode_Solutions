07ac513df623084fb790a7fc8b7062b977f273d0
GUHAN S <122159050+Guhan-S-03@users.noreply.github.com>
2024-01-26 21:37:18 +0530

    Time: 85 ms (75.43%) | Memory: 21.9 MB (46.53%) - LeetSync

---
+++ 576-out-of-boundary-paths/out-of-boundary-paths.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findPaths(self, m: int, n: int, maxMove: int, startRow: int, startColumn: int) -> int:
+        rows,cols=m,n
+        mod=10**9+7
+        cache={}
+
+        def dfs(r,c,moves):
+            if(r<0 or r==rows or c<0 or c==cols ):
+                return 1
+            if(moves==0):
+                return 0
+            if (r,c,moves) in cache:
+                return cache[(r,c,moves)]
+            cache[(r,c,moves)]=((dfs(r+1,c,moves-1)+dfs(r-1,c,moves-1))%mod+(dfs(r,c+1,moves-1)+dfs(r,c-1,moves-1))%mod)%mod
+            return cache[(r,c,moves)]
+        return dfs(startRow,startColumn,maxMove)
+        
