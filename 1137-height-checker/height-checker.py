e22c47c2c5869e59bb13d628d7190072f477116e
GUHAN S <122159050+Guhan-S-03@users.noreply.github.com>
2024-06-10 09:50:04 +0530

    Time: 38 ms (64.91%) | Memory: 16.6 MB (29.57%) - LeetSync

--- 1137-height-checker/height-checker.py
+++ 1137-height-checker/height-checker.py
@@ -1,8 +1,6 @@
 class Solution:
     def heightChecker(self, heights: List[int]) -> int:
-        expected=heights.copy()
-        expected.sort()
-
+        expected=sorted(heights)
         mismatch=0
         for i in range(len(heights)):
             if heights[i]!=expected[i]:
