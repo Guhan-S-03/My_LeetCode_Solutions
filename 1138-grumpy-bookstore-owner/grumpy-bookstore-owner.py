aba0bd1f963e46fa986ba37016d83b1ac570781a
GUHAN S <122159050+Guhan-S-03@users.noreply.github.com>
2024-06-21 19:08:10 +0530

    Time: 217 ms (54.69%) | Memory: 18.9 MB (38.37%) - LeetSync

---
+++ 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.py
@@ -0,0 +1,23 @@
+class Solution:
+    def maxSatisfied(self, customers: List[int], grumpy: List[int], minutes: int) -> int:
+        intial_satisf=0
+        for i in range(len(grumpy)):
+            if grumpy[i]==0:
+                intial_satisf+=customers[i]
+        
+        max_cust,secret_window=0,intial_satisf
+
+        for i in range(minutes):
+            if grumpy[i]==1:
+                secret_window+=customers[i]
+        max_cust=secret_window
+        for j in range(1,(len(grumpy)-minutes)+1):
+            if grumpy[j-1]==1:
+                secret_window-=customers[j-1]
+            if grumpy[j+minutes-1]==1:
+                secret_window+=customers[j+minutes-1]
+                max_cust=max(max_cust,secret_window)
+        return max_cust
+
+
+        
