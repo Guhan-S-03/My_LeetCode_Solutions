655c9a819cc01252dd126d722dc4a8ffdda5c5b8
GUHAN S <122159050+Guhan-S-03@users.noreply.github.com>
2024-06-25 13:04:00 +0530

    Time: 34 ms (73.41%) | Memory: 16.5 MB (74.62%) - LeetSync

---
+++ 1114-binary-search-tree-to-greater-sum-tree/binary-search-tree-to-greater-sum-tree.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def bstToGst(self, root: TreeNode) -> TreeNode:
+        ##actually we need to update every val by the val which is greater than them
+        #so we can do inorder traversal to make them ascd but actually we can do the 
+        #reverse of that reverse inorder traversal to update the global totsum
+        #var and update every node and totsum val so that we can update every val from
+        #right--root--left
+        #it is same as 538
+        cursum=0
+
+        def dfs(node):
+            nonlocal cursum
+            if not node:
+                return 
+            
+            dfs(node.right)
+            temp=node.val
+            node.val+=cursum
+            cursum+=temp
+            dfs(node.left)
+        dfs(root)
+        return root
+        
