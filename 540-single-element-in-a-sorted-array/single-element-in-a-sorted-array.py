7ffcafb3f95dd5e9ce213ea90a4d9fc1746ff438
GUHAN S <122159050+Guhan-S-03@users.noreply.github.com>
2024-07-03 20:45:22 +0530

    Time: 142 ms (38.01%) | Memory: 26.6 MB (46.94%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
@@ -1,11 +1,26 @@
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
-        i=0
+        ##actually first time i solved it using a while loop
+        #but they want it in a logn time so obviously binary search
+        #the key here is the target has diff values on both sides
+        #so we can make use of it chk mid and shift over to the side which has odd ele
+        #becoz only one odd target here
 
-        while i<len(nums):
-            if i+1<len(nums) and nums[i]==nums[i+1]:
-                i+=2
+        l,r=0,len(nums)-1
+
+        while l<=r:
+            m=l+((r-l)//2) ##difference of r-l so no out of bounce
+
+            #since the target may be in first and lst pos
+            if (m-1<0 or nums[m]!=nums[m-1]) and (m+1==len(nums) or nums[m]!=nums[m+1]):
+                return nums[m]
+
+            left_elements= m-1 if nums[m]==nums[m-1] else m
+
+            if left_elements % 2:
+                r=m-1
             else:
-                return nums[i]
+                l=m+1
         
+
         
