f8b36e843537502ff7f383ed1287f7168ca0ab6c
GUHAN S <122159050+Guhan-S-03@users.noreply.github.com>
2024-09-20 18:36:02 +0530

    Time: 40 ms (69.29%) | Memory: 16.6 MB (18.80%) - LeetSync

--- 441-arranging-coins/arranging-coins.py
+++ 441-arranging-coins/arranging-coins.py
@@ -1,10 +1,20 @@
 class Solution:
     def arrangeCoins(self, n: int) -> int:
-        stair=1
-        while n>=stair:
-            n-=stair
-            stair+=1
-        return stair-1
+        ##so the simple iteration for every step also works and find the answer
+        #but here we are using the guass formula to find the no of rows we can complete
+
+        l,r=1,n
+        res=0
+
+        while l<=r:
+            mid=(l+r)//2
+            req_coins=(mid/2)*(mid+1)
+            if req_coins>n:
+                r=mid-1
+            else:
+                res=max(res,mid)
+                l=mid+1
+        return res 
 
         
 
